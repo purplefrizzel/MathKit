21a7cf46faf26176a114565b5319b6e7b5496f57
Lewis Shaw <lewisthe14th@icloud.com>
2019-10-17 22:25:20 +0100

    Added Indices.

---
+++ MathKit/Indices.swift
@@ -0,0 +1,20 @@
+//
+//  Indices.swift
+//  MathKit
+//
+//  Created by Lewis Shaw on 17/10/2019.
+//  Copyright © 2019 Lewis Shaw. All rights reserved.
+//
+
+import Foundation
+
+
+extension Int {
+    var square: Any { return self * self }
+}
+
+extension Int {
+    var cube: Any { return self * self * self }
+}
+
+
