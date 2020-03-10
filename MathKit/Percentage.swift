e627e646d008fe219e3416e6c3da1d1586776a7f
Lew <45900375+purplefrizzel@users.noreply.github.com>
2020-03-10 19:20:07 +0000

    Update Percentage.swift

--- MathKit/Percentage.swift
+++ MathKit/Percentage.swift
@@ -10,7 +10,7 @@ import Foundation
 
 extension Double {
     /// Returns a clean percentage as a String.
-    var clean: String { return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self) }
+    var clean: String { return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self): String(self) }
 }
 
 extension Double {
